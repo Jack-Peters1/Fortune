d1ae881c70444d20f65284c85ef331abecabba2c
jhuff <jhuff@potomacschool.org>
2021-12-10 14:22:53 -0500

    Disconnect Message

--- server.py
+++ server.py
@@ -29,7 +29,10 @@ def handle_client(conn, addr):
 
             with clients_lock:
                 for client in clients:
-                    client.sendall(msg.encode(FORMAT))
+                    if msg is not DISCONNECT_MESSAGE:
+                        client.sendall(msg.encode(FORMAT))
+                    else:
+                        client.sendall((str(addr) + "has disconnected.").encode(FORMAT))
 
             if msg == DISCONNECT_MESSAGE:
                 connected = False

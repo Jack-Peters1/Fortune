deaaa09a56bbe4d95c62c1b4a4d22f1962d701a2
jhuff <jhuff@potomacschool.org>
2021-12-16 11:11:10 -0500

    PM by ID instead of Default Gateway

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import threading
 
 HEADER = 64
 PORT = 5050
-SERVER = ""
+SERVER = "10.1.136.87"
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
@@ -11,6 +11,7 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create Socket server
 server.bind(ADDR)  # Bind server to the target address
 
+client_ids = []
 clients = set()  # List of all clients. Part of PMs and also part of broadcasting to all clients.
 clients_lock = threading.Lock()  # Useful for making all threads accessible from all threads so that the all clients can be send broadcasts
 
@@ -19,16 +20,15 @@ def handle_client(conn, addr):
     print("New connection. " + str(addr) + " connected.")
     with clients_lock:
         clients.add(conn)  # Add the new client to the list
-
+        client_ids.append(conn.getpeername()[0])
+    print(client_ids)
     with clients_lock:
         for client in clients:
             client.sendall(("A new client has connected. Active connections: " + str(threading.active_count() - 1)).encode(FORMAT))  # Broadcast to all clients when a new user has connected
 
     connected = True
     while connected:  # While each connection is established...
-        target = conn.recv(HEADER).decode(FORMAT)  # Begin searching for messages. The first message, target, is for the IP that the server will try to PM.
-                                                   # If this IP is null, it will instead broadcast the message to all clients. IPs should be the Default Gateway
-                                                   # On Windows, your default gateway can be found by running ipconfig in terminal.
+        target = conn.recv(HEADER).decode(FORMAT)  # Begin searching for messages. The first message, target, is for the ID that the server will try to PM.
         print(target)
         msg_length = conn.recv(HEADER).decode(FORMAT)  # The second message will always be the length of the third comment so that the server always knows how much data to request from the client
         if msg_length:  # If the message is not empty, continue
@@ -36,23 +36,23 @@ def handle_client(conn, addr):
             msg = conn.recv(msg_length).decode(FORMAT)  # Decode the incoming message which was sent in UTF-8 format
             print(str(addr) + " " + str(msg))
 
-            with clients_lock:  # For all of the clients in all threads...
-                for client in clients:
-                    if msg != DISCONNECT_MESSAGE:  # If the client is not trying to disconnect...
-                        if target == "null":  # If there is no specific target for the message, broadcast to all clients.
-                            client.sendall(msg.encode(FORMAT))
-                        elif client.getpeername()[0] == addr[0]:  # Otherwise, Send the PM back to the sender so that it shows up in the chatbox.
-                            client.send(str(msg + " [PRIVATE MESSAGE]").encode(FORMAT))
-                        if client.getpeername()[0] == target:  # And also Send the PM to the target and only to the target.
-                            print("Client to PM Found")
-                            client.send(str(msg + " [PRIVATE MESSAGE]").encode(FORMAT))
-                    else:  # If the client is trying to disconnect and the disconnect message has been recieved by the server, notify all clients still connected that a disconnect has occurred.
-                        client.sendall(("A user has disconnected. Active connections: " + str(threading.active_count() - 2)).encode(FORMAT))
-
             if msg == DISCONNECT_MESSAGE:
                 connected = False  # Stop the loop
                 with clients_lock:  # Remove the clients from the client list
                     clients.remove(conn)
+
+            with clients_lock:  # For all of the clients in all threads...
+                for client in clients:
+                    if msg != DISCONNECT_MESSAGE:  # If the client is not trying to disconnect...
+                        if target == "null":  # If there is no specific target for the message, broadcast to all clients.
+                            client.sendall(str("ID: " + str(client_ids.index(conn.getpeername()[0])) + " " +msg).encode(FORMAT))
+                        elif client.getpeername()[0] == addr[0]:  # Otherwise, Send the PM back to the sender so that it shows up in the chatbox.
+                            client.send(str("ID: " + str(client_ids.index(conn.getpeername()[0])) + " " +msg + " [PRIVATE MESSAGE]").encode(FORMAT))
+                        if client_ids.index(conn.getpeername()[0]) == target:  # And also Send the PM to the target and only to the target.
+                            print("Client to PM Found")
+                            client.send(str("ID: " + str(client_ids.index(conn.getpeername()[0])) + " " +msg + " [PRIVATE MESSAGE]").encode(FORMAT))
+                    else:  # If the client is trying to disconnect and the disconnect message has been recieved by the server, notify all clients still connected that a disconnect has occurred.
+                        client.sendall(("A user has disconnected. Active connections: " + str(threading.active_count() - 2)).encode(FORMAT))
 
     conn.close()  # close the connection to the client
 
